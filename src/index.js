8ff2f68f51621999c3a71d030456f51d3de8c838
harvis gomez <harvisgomez17@gmail.com>
2024-12-04 22:38:52 -0400

    agregando funciones al js para la captura de los valores enviados en las notas, ademas de un css simple para las pruebas del mismo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import express from "express"
-import { Server as webSocketServer  } from "socket.io";
-import http from "https"
+import { Server as WebSocketServer  } from "socket.io";
+import http from "http"
+
+const notes = {}
 
 const app = express();
 const server = http.createServer(app)
-const io = new webSocketServer(server);
+const io = new WebSocketServer(server);
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
 io.on('connection', (socket)=>{
-    console.log('new connection:', socket.id)
-    socket.emit('ping');
+    console.log('new connection:', socket.id);
+
+    socket.on('client:newnote', data => {
+        console.log(data)
+    })
 });
 
 server.listen(3000);
--- src/public/app.js
+++ src/public/app.js
@@ -1,5 +1,14 @@
 const socket = io();
 
-socket.on('ping', ()=>{
-    console.log('listened');
+const noteForm = document.querySelector('#note-form');
+const tittle = document.querySelector('#tittle')
+const description = document.querySelector('#description')
+
+noteForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    socket.emit('client:newnote', {
+        tittle: tittle.value,
+        description: description.value
+    })
 })
