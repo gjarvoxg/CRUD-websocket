14b8478b19e382b99a50302264aa9c6a01fe9a9e
gjarvoxg <harvisgomez17@gmail.com>
2024-12-06 13:36:43 -0400

    actualizacion - login en contruccion

--- src/public/ui.js
+++ src/public/ui.js
@@ -7,15 +7,15 @@ const noteUI = notes => {
     const div = document.createElement('div')
 
     div.innerHTML = `
-    <div class="card card -body rounded-0 mb-2 animate__animated animate__fadeInLeft">
-        <div class="d-flex justify-content-between ">
+    <div class="card card -body rounded-2 mb-2 animate__animated animate__lightSpeedInLeft">
+        <div class="d-flex justify-content-between p-2">
             <h1 class="h3 card-title">${notes.title}</h1>
             <div>
                 <button class="btn btn-danger delete" data-id="${notes.id}"> delete </button>
                 <button class="btn btn-secondary update" data-id="${notes.id}"> update </button>
             </div>
-        </div>
-        <p>${notes.description}</p>
+        </div >
+        <p class="notes-description">${notes.description}</p>
     </div>
     `
     const btnDelete = div.querySelector('.delete')
